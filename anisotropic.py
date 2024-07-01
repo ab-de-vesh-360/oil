f5b77375a1d9d15cb7eb1a5b65049ba03351a95b
ab-de-vesh-360 <deveshsingh7909@gmail.com>
2024-07-01 22:01:58 +0530

    Add files via upload

---
+++ anisotropic.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
+st.title('2D Anisotropic Permability')
+st.sidebar.title('Inputs')
+
+kh= st.sidebar.slider('Horizontal Perm(md)',min_value = 100, max_value= 200, value= 200)
+kt= st.sidebar.slider('Transverse Perm(md)',min_value = 0, max_value= 50, value= 50)
+
+x = np.linspace(0,90,30)
+y = np.radians(x)
+k11 = kh*(np.cos(y))**2 + kt*(np.sin(y))**2
+k22 = kh*(np.sin(y))**2 + kt*(np.cos(y))**2
+k12_21 = (kh - kt)*(np.cos(y))*(np.sin(y))
+
+b = st.button('Show Permeability Profile')
+
+if b:  #tells if the button is pressed
+    plt.style.use('classic')
+    plt.figure(figsize=(8,6))
+    fig,ax = plt.subplots()
+
+    ax.plot(x,k11, label = 'k11')
+    ax.plot(x,k22, label = 'k22')
+    ax.plot(x,k12_21, label = 'k12_21')
+    
+    ax.grid(True)
+    ax.set_xlabel('Angle(Degree)')
+    ax.set_ylabel('Permeabilities Tensor Elements(md)')
+    ax.legend()
+    ax.set_title('Permeability Profile')
+    
+
+    st.pyplot(fig)  #for showing the fig in app    
