de12e2ca3afb6a0e5643131269f73e4e94d18eeb
ab-de-vesh-360 <deveshsingh7909@gmail.com>
2024-07-05 00:09:12 +0530

    anisotropic.py

--- anisotropic.py
+++ anisotropic.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title('2D Anisotropic Permability')
 st.sidebar.title('Inputs')
