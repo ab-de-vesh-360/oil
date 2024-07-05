df3c452b064273f3b86b4d54211b529a63bd1e04
ab-de-vesh-360 <deveshsingh7909@gmail.com>
2024-07-05 14:08:16 +0530

    anisotropic.py

--- anisotropic.py
+++ anisotropic.py
@@ -1,9 +1,11 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import pandas as pd
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+
 
 st.title('2D Anisotropic Permability')
 st.sidebar.title('Inputs')
@@ -21,18 +23,35 @@ b = st.button('Show Permeability Profile')
 
 if b:  #tells if the button is pressed
     plt.style.use('classic')
-    plt.figure(figsize=(8,6))
+    plt.figure(figsize=(6,8))
     fig,ax = plt.subplots()
 
     ax.plot(x,k11, label = 'k11')
     ax.plot(x,k22, label = 'k22')
     ax.plot(x,k12_21, label = 'k12_21')
     
-    ax.grid(True)
-    ax.set_xlabel('Angle(Degree)')
-    ax.set_ylabel('Permeabilities Tensor Elements(md)')
-    ax.legend()
-    ax.set_title('Permeability Profile')
-    
+    #ax.grid(True)
+    #ax.set_xlabel('Angle(Degree)')
+    #ax.set_ylabel('Permeabilities Tensor Elements(md)')
+    #ax.legend()
+    #ax.set_title('Permeability Profile')
 
-    st.pyplot(fig)  #for showing the fig in app    
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=x, y=k11, mode='lines+markers', name='k11',
+                         hovertemplate='Angle: %{x}<br>k11: %{y}'))
+    fig.add_trace(go.Scatter(x=x, y=k22, mode='lines+markers', name='k22',
+                         hovertemplate='Angle: %{x}<br>k22: %{y}'))
+    fig.add_trace(go.Scatter(x=x, y=k12_21, mode='lines+markers', name='k12_21',
+                         hovertemplate='Angle: %{x}<br>k12_21: %{y}'))
+    hovermode='closest'
+    
+    fig.update_layout(
+    title='2D Permeability Profile',
+    xaxis_title='Angle (degrees)',
+    yaxis_title='Permeabilities Tensor Elements(md)',
+    hovermode='closest')
+    st.plotly_chart(fig)
+    
+    #st.pyplot(fig)
+      #for showing the fig in app    
