2696beae8f33879ce7189088b699d12d943fa210
ab-de-vesh-360 <deveshsingh7909@gmail.com>
2024-07-06 16:51:22 +0530

    dc.py

--- dc.py
+++ dc.py
@@ -224,7 +224,7 @@ elif st.session_state.page == 'upload_csv':
         
         future_time_period = st.sidebar.number_input('Forecast Time Period (years)', value=10, min_value=1, step=1)
         d = st.sidebar.button('Show Production Forecast')
-            if d:
+        if d:
                 future_t = np.linspace(t[-1], t[-1] + future_time_period, 100)
 
                 if best_fit == 'Exponential':
